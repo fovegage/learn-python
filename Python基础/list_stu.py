d5b22eedf2d28bbc14afc975cd84c8aa2b7ee5ae
fovegage <fovegage@gmail.com>
2018-09-20 13:13:22 +0800

    process

---
+++ Python基础/list_stu.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/16 17:24
+# @Author  : fovegage
+# @Email   : fovegage@gmail.com
+# @File    : 91_pron.py
+# @Software: PyCharm
+
+
+"""
+class str(object)
+"""
+str = "我爱你中国/"
+# 如果未指定，删除空格；指定则删除指定字符
+print(str.rstrip('/'))
--- Python基础/string_stu.py
+++ Python基础/string_stu.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2018/9/16 17:24
+# @Time    : 2018/9/20 11:21
 # @Author  : fovegage
 # @Email   : fovegage@gmail.com
-# @File    : 91_pron.py
+# @File    : String__stu.py
 # @Software: PyCharm
-
-
-"""
-class str(object)
-"""
-str = "我爱你中国/"
-# 如果未指定，删除空格；指定则删除指定字符
-print(str.rstrip('/'))
