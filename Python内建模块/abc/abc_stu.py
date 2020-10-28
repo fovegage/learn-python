d5c2cacd25f9aef1c44a4da04cc60adc4fc6d9aa
fovegage <fovegage@gmail.com>
2020-10-28 10:41:44 +0800

    Python abc abstract class

--- Python内建模块/abc/abc_stu.py
+++ Python内建模块/abc/abc_stu.py
@@ -4,3 +4,25 @@
 # @Email   : fovegage@gmail.com
 # @File    : abc_stu.py
 # @Software: PyCharm
+import abc
+import six
+
+
+# 类内 class 如何调用  研究Django Meta的实现
+# abc.
+@six.add_metaclass(abc.ABCMeta)  # 声明后子类必须实现  不然会报错
+class BaseClass:
+    @abc.abstractmethod
+    def fun_a(self):
+        """
+        implement
+        """
+
+
+class ImplClass(BaseClass):
+    def fun_a(self):
+        print("I'm implemented")
+
+
+impl = ImplClass()
+impl.fun_a()
